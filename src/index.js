dd080ccbfbc0077bb1c8a7a5255b279f06e22458
Cheenee <cheenee.colaste24@gmail.com>
2017-08-20 06:25:08 +0800

    webpack real project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import sum from './sum';
-import './image_viewer';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Routes from './router';
+import '../style/materialize.css';
+import '../style/react-range.css';
+import '../style/style.css';
+console.log("hi");
 
-const total = sum(10,5);
-console.log(total);
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        /**
-         * publicPath: this is important to all the build file
-         * specially in the separate build images.
-         * This will prepend all the build file path
-         */
-        publicPath: 'build/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    loader: 'css-loader'
-                })
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        /**
-                         * option limit: every images that 40000 kb larger 
-                         * will not be include in the bundle.js, 
-                         * it will build in a separate hash image file
-                         */
-                        options: { limit: 40000 }
-                    },
-                    'image-webpack-loader'
-                ]
-            }
+const VENDOR_LIBS = [
+  "faker",
+  "lodash",
+  "react",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "react-router",
+  "redux",
+  "redux-form",
+  "redux-thunk",
+];
 
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css')
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'//name will replace the key from the Entry section
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
+  },
+  plugins: [
+    /** 
+     * this will exclude or remove duplicate
+     * the vendor files in the bundle.js
+    */
+    new webpack.optimize.CommonsChunkPlugin({
+      /** manifest tells weather a vendor file has change */
+      names: ['vendor', 'manifest']
+    }),
+    /**
+     * this will automatically include
+     * dynamic script src in the build
+     */
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
 
+  ]
 };
-
-
-module.exports = config;
